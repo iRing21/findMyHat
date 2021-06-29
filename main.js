57dea942a6abd6d3dce07e75a9307c7696cb0c14
iRing21 <ian.ring21@gmail.com>
2021-06-29 14:51:52 -0400

    playGame in the works

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const pathCharacter = '*';
 class Field {
     constructor(fieldArray){
         this._fieldArray = fieldArray;
+        this.x = 0;
+        this.y = 0;
     }
 
     print() {
@@ -17,10 +19,65 @@ class Field {
         }
       }
 
-    playGame() {
-        const input = prompt('Enter a travel direction, N S E or W');
-        
+    gameOverChecker(input) {
+        if (input === undefined){
+            console.log('You went out of bounds, n00b, try again');
+            return false;            
+        }
+        else if (input === hole){
+            console.log('You fell in a hole, you\'re human garbage');
+            return false; 
+        }
+        else if (input === hat){
+            console.log('You found your hat, suprising');
+            return false;
+        }
+        else{
+            return true; 
+        }  
     }
+
+    playGame() {
+        this.print();
+        while(this.gameOverChecker(this._fieldArray[this.x][this.y]) !== false){
+            const playerInput = prompt('Enter a travel direction, N S E or W');
+            switch (playerInput.toUpperCase()){
+            case 'N':
+                this.x -= 1;
+                this._fieldArray[this.x][this.y] = pathCharacter;
+                this._fieldArray[this.x+1][this.y] = fieldCharacter;
+                this.print();
+                break;
+
+            case 'S':
+                this.x += 1;
+                this._fieldArray[this.x][this.y] = pathCharacter;
+                this._fieldArray[this.x-1][this.y] = fieldCharacter;
+                this.print();
+                break;
+
+            case 'E':
+                this.y += 1;
+
+                this._fieldArray[this.x][this.y] = pathCharacter;
+                this._fieldArray[this.x][this.y-1] = fieldCharacter;
+                this.print();
+                break;
+                
+            case 'W':
+                this.y -= 1;
+                this._fieldArray[this.x][this.y] = pathCharacter;
+                this._fieldArray[this.x][this.y+1] = fieldCharacter;
+                this.print();
+                break;
+            
+            default: 
+                console.log('Illegal input :(');
+            }
+        }
+    }
+
+
 }
 
 const myField = new Field([
@@ -30,6 +87,4 @@ const myField = new Field([
   ]);
 
 
-
-
-console.log(myField.print());
+myField.playGame();
