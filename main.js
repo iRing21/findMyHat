3411327cd88fd0b187ad988de2531b4209d4c728
iRing21 <ian.ring21@gmail.com>
2021-06-29 13:31:22 -0400

    print method added

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+
+class Field {
+    constructor(fieldArray){
+        this._fieldArray = fieldArray;
+    }
+
+    print() {
+        for (let row of this._fieldArray){
+          console.log(row.join(' '));
+        }
+      }
+
+    playGame() {
+        const input = prompt('Enter a travel direction, N S E or W');
+        
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+
+
+
+console.log(myField.print());
